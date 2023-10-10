95c3092031d6ded3fa250ce0e257c0d693c7199f
Ulises <robdiaz247@gmail.com>
2023-10-10 00:12:20 -0300

    favicon

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
   return (
     <div className="footerContainer">
       <div className="footerLogo"></div>
-      <div className="footerTerms"><p>* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fringilla consectetur metus at scelerisque. Nam mattis felis quis sollicitudin cursus. Suspendisse potenti. Vestibulum ante ipsum primis in faucibus orci luctus.</p><div className="footerPrivacyTermsContainer"><p className="footerPrivacyTerms">Privacy Policy </p><p className="footerPrivacyTerms"> Terms & Conditions</p><p className="footerPrivacyTerms" onClick={() => router.push("/AdminPanel")}>Admin</p></div>
+      <div className="footerTerms"><div className="footerPrivacyTermsContainer"><p className="footerPrivacyTerms">Privacy Policy </p><p className="footerPrivacyTerms"> Terms & Conditions</p><p className="footerPrivacyTerms" onClick={() => router.push("/AdminPanel")}>Admin</p></div>
       <p className="footerPrivacyTerms"></p></div>
-      <div className="footerCopy"><p>© Copyright 2023 Neurolinguas all rights reserved</p></div>
+      <div className="footerCopy"><p>© Copyright 2023 Anglohispana all rights reserved</p></div>
     </div>
   );
 };
